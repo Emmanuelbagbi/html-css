fd6a01989231008aa4fa80e63cddd34c1456aacc
Emmanuelbagbi <emmanuelbagbi4@gmail.com>
2024-03-20 04:50:20 -0700

    js operator

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,29 @@
+// let x= "Hello"
+// let y="world"
+// console.log(x+y)
+
+// let x="Hello"
+// let y=10
+// let sum = x+y
+// console.log(x+y)
+
+// let a = 1
+// // let b = ++a;
+// let c = --a;
+// console.log(b);
+// // console.log(c)
+
+// let num1 = 5;
+// let num2 = 3;
+// // num1 += num2;
+// num1 = num1 + num2;
+// console.log(num1)
+
+let a = true;
+let b = "Hello";
+console.log(a+b)
+
+let num1 = 22;
+let num2 = 10;
+let sum = num1 + num2
+console.log(sum)
