29f7f4ee5701d2872686502e75105eec3f4ed777
Emmyboi <160488425+Emmanuelbagbi@users.noreply.github.com>
2024-04-22 18:32:21 +0100

    Add files via upload

---
+++ Sign Up.js
@@ -0,0 +1,11 @@
+
+
+function headerboth1(name) {
+    name = prompt("Enter name: ");
+    if (!name){
+        alert("Pleasse you need to Sign Up: ");
+        return;
+    }else{
+        document.write('Thanks but still working on my Next page')
+    }
+}
